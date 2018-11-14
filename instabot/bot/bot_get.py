f9c198abee0376c711012fba1220eb544f807216
Naman Patwari <thenamanpat@gmail.com>
2018-11-14 17:15:12 -0500

    Revert "use userd_id"
    This reverts commit 7a63f09bb9f924d3f14894c32b0e096e75bbe7a0.

--- instabot/bot/bot_get.py
+++ instabot/bot/bot_get.py
@@ -54,14 +54,6 @@ def get_user_medias(self, user_id, filtration=True, is_comment=False):
         return []
     return self.filter_medias(self.api.last_json["items"], filtration, is_comment=is_comment)
 
-def get_user_id_medias(self, user_id, filtration=True, is_comment=False):
-    # user_id = self.convert_to_user_id(user_id)
-    self.api.get_user_feed(user_id)
-    if self.api.last_json["status"] == 'fail':
-        self.logger.warning("This is a closed account.")
-        return []
-    return self.filter_medias(self.api.last_json["items"], filtration, is_comment=is_comment)
-
 
 def get_total_user_medias(self, user_id):
     user_id = self.convert_to_user_id(user_id)
@@ -185,36 +177,15 @@ def get_user_info(self, user_id, use_cache=True):
         self._user_infos[user_id] = user_info
     return self._user_infos[user_id]
 
-def get_user_id_info(self, user_id, use_cache=True):
-    # user_id = self.convert_to_user_id(user_id)
-    if not use_cache or user_id not in self._user_infos:
-        self.api.get_username_info(user_id)
-        last_json = self.api.last_json
-        if last_json is None or 'user' not in last_json:
-            return False
-        user_info = last_json['user']
-        self._user_infos[user_id] = user_info
-    return self._user_infos[user_id]
-
 
 def get_user_followers(self, user_id, nfollows):
     user_id = self.convert_to_user_id(user_id)
     followers = self.api.get_total_followers(user_id, nfollows)
     return [str(item['pk']) for item in followers][::-1] if followers else []
 
-def get_user_id_followers(self, user_id, nfollows):
-    # user_id = self.convert_to_user_id(user_id)
-    followers = self.api.get_total_followers(user_id, nfollows)
-    return [str(item['pk']) for item in followers][::-1] if followers else []
-
 
 def get_user_following(self, user_id, nfollows=None):
     user_id = self.convert_to_user_id(user_id)
-    following = self.api.get_total_followings(user_id, nfollows)
-    return [str(item['pk']) for item in following][::-1] if following else []
-
-def get_user_id_following(self, user_id, nfollows=None):
-    # user_id = self.convert_to_user_id(user_id)
     following = self.api.get_total_followings(user_id, nfollows)
     return [str(item['pk']) for item in following][::-1] if following else []
 
--- instabot/bot/bot_unfollow.py
+++ instabot/bot/bot_unfollow.py
@@ -23,28 +23,6 @@ def unfollow(self, user_id):
         self.logger.info("Out of unfollows for today.")
     return False
 
-def unfollow_id(self, user_id):
-    # user_id = self.convert_to_user_id(user_id)
-    user_info = self.get_user_info(user_id)
-    username = user_info["username"]
-    self.console_print('===> Going to unfollow `user_id`: {} with username: {}'.format(user_id, username))
-
-    if self.check_user(user_id, unfollowing=True):
-        return True  # whitelisted user
-    if not self.reached_limit('unfollows'):
-        self.delay('unfollow')
-        if self.api.unfollow(user_id):
-            msg = '===> Unfollowed, `user_id`: {}, user_name: {}'
-            self.console_print(msg.format(user_id, username), 'yellow')
-            self.unfollowed_file.append(user_id)
-            self.total['unfollows'] += 1
-            if user_id in self._following:
-                self._following.remove(user_id)
-            return True
-    else:
-        self.logger.info("Out of unfollows for today.")
-    return False
-
 
 def unfollow_users(self, user_ids):
     broken_items = []
